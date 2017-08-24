3fad0190295e6b5f8edf72b684326398074edbca
Samuel Muñoz <yhau1989@gmail.com>
2017-08-24 16:05:37 -0500

    fuentes iniciales
    Permite ver las cloas de impresion de windows (7,8,10).

---
+++ TestPaperPrint/TestPaperPrint/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace TestPaperPrint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_printers = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(306, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Test Paper Printer";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Test Paper";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_printers
+            // 
+            this.lbl_printers.AutoSize = true;
+            this.lbl_printers.Location = new System.Drawing.Point(48, 191);
+            this.lbl_printers.Name = "lbl_printers";
+            this.lbl_printers.Size = new System.Drawing.Size(0, 13);
+            this.lbl_printers.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 449);
+            this.Controls.Add(this.lbl_printers);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_printers;
+    }
+}
+
---
+++ TestPaperPrint/TestPaperPrint/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestPaperPrint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // Puedes utilizar WMI para obtener la informacion del servicio con algo asi
+
+            try
+            {
+                string PrinterJobs = "SELECT * FROM Win32_PrintJob";
+
+                ManagementObjectSearcher FindPrintJobs = new ManagementObjectSearcher(PrinterJobs);
+                ManagementObjectCollection prntJobCollection = FindPrintJobs.Get();
+
+                lbl_printers.Text = "";
+
+                foreach (ManagementObject prntJob in prntJobCollection)
+                {
+
+                    System.String jobName = prntJob.Properties["Name"].Value.ToString();
+                    //lbl_printers.Text += jobName + "\n";
+
+                    char[] JobSplit = new char[1];
+                    JobSplit[0] = Convert.ToChar(",");
+                    string prnterName = jobName.Split(JobSplit)[0];
+                    lbl_printers.Text += "\t\t printerName: " + prnterName + "\n";
+                    string documentName = "Doucment Name->" + prntJob.Properties["Document"].Value.ToString() + " Sender Name->" + prntJob.Properties["owner"].Value.ToString();
+                    lbl_printers.Text += "\t\t documentName: " + documentName + "\n";
+
+
+                    /*if (String.Compare(prnterName, printerName, true) == 0)
+                    {
+                        printJobCollection.Add(documentName);
+                    }*/
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                lbl_printers.Text = ex.Message;
+            }
+        }
+
+       
+    }
+}
